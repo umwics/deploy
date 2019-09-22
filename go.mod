55951623a66c813e21a3c9a3762c40814b62b778
Chris de Graaf <chrisadegraaf@gmail.com>
2019-09-22 14:12:17 +0700

    Implement most of the logic

---
+++ go.mod
@@ -0,0 +1,14 @@
+module deploy
+
+go 1.13
+
+require (
+	github.com/aws/aws-lambda-go v1.13.2
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/frankban/quicktest v1.5.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/mholt/archiver v3.1.1+incompatible
+	github.com/nwaples/rardecode v1.0.0 // indirect
+	github.com/pierrec/lz4 v2.3.0+incompatible // indirect
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+)
--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/mholt/archiver"
 )
 
-// SSHKey is the path to our SSH key.
-const SSHKey = "ssh/wics"
+const (
+	// RepoOwner is the owner of the repository.
+	RepoOwner = "umwics"
+	// RepoName is the name of the repository.
+	RepoName = "wics-site"
+	// RemotePath is the path to the site on the remote server.
+	RemotePath = "wics@aviary.cs.umanitoba.ca:~/public_html"
+	// SSHKey is the path to our SSH key.
+	SSHKey = "ssh/wics"
+)
 
-// WebhookSecret is the secret for GitHub.
-var WebhookSecret = []byte(os.Getenv("WEBHOOK_SECRET"))
+var (
+	// RepoURL is the download URL of the repo.
+	RepoURL = fmt.Sprintf("https://github.com/%s/%s/archive/master.zip", RepoOwner, RepoName)
+	// WebhookSecret is the secret for GitHub.
+	WebhookSecret = []byte(os.Getenv("WEBHOOK_SECRET"))
+)
 
 type (
 	// Request is the type we get from Lambda.
@@ -23,19 +39,106 @@ type (
 	Response events.APIGatewayProxyResponse
 )
 
+// Validate validates the request.
+func (req Request) Validate(resp *Response) bool {
+	return true // TODO
+}
+
+// downloadRepo downloads and unzip the repository, and returns its directory.
+func downloadRepo() (string, error) {
+	resp, err := http.Get(RepoURL)
+	if err != nil {
+		return "", err
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	f, err := ioutil.TempFile("", "wics.*.zip")
+	if err != nil {
+		return "", err
+	}
+	defer os.Remove(f.Name())
+
+	if _, err = f.Write(b); err != nil {
+		return "", err
+	}
+
+	dir, err := ioutil.TempDir("", "wics")
+	if err != nil {
+		return "", err
+	}
+
+	if err = archiver.Unarchive(f.Name(), dir); err != nil {
+		return "", err
+	}
+
+	return filepath.Join(dir, fmt.Sprintf("%s-master", RepoName)), nil
+}
+
+// buildSite builds the site with Jekyll.
+func buildSite(dir string) (string, error) {
+	if err := doCmd(dir, "jekyll", "build"); err != nil {
+		return "", err
+	}
+	return filepath.Join(dir, "_site"), nil
+}
+
+// syncSite pushes the site to the remote server with rsync.
+func syncSite(dir string) error {
+	defer os.RemoveAll(dir)
+
+	// To recursively sync the directory, the path must end with a slash.
+	if !strings.HasSuffix(dir, "/") {
+		dir += "/"
+	}
+
+	// We need to use a custom SSH command to use our bundled SSH key.
+	sshCmd := fmt.Sprintf("ssh -i %s", SSHKey)
+
+	return doCmd("rsync", "-e", sshCmd, "-a", "--delete", dir, RemotePath)
+}
+
+// doCmd runs some shell command and prints its output.
+func doCmd(dir, program string, args ...string) error {
+	cmd := exec.Command(program, args...)
+	if dir != "" {
+		cmd.Dir = dir
+	}
+	b, err := cmd.CombinedOutput()
+	if len(b) > 0 {
+		fmt.Println(string(b))
+	}
+	return err
+}
+
 func main() {
 	lambda.Start(func(req Request) (resp Response, nillErr error) {
-		if b, err := exec.Command("rsync", "--version").CombinedOutput(); err != nil {
-			log.Println("rsync:", err)
-		} else {
-			fmt.Println(string(b))
+		if req.Validate(&resp) {
+			resp.StatusCode = 500
+
+			dir, err := downloadRepo()
+			if err != nil {
+				resp.Body = fmt.Errorf("download repo: %w", err).Error()
+				return
+			}
+
+			dir, err = buildSite(dir)
+			if err != nil {
+				resp.Body = fmt.Errorf("build site: %w", err).Error()
+				return
+			}
+
+			if err = syncSite(dir); err != nil {
+				resp.Body = fmt.Errorf("sync site: %w", err).Error()
+				return
+			}
+
+			resp.StatusCode = 200
 		}
-		if b, err := exec.Command("jekyll", "-v").CombinedOutput(); err != nil {
-			log.Println("jekyll:", err)
-		} else {
-			fmt.Println(string(b))
-		}
-		resp.StatusCode = 200
+
 		return
 	})
 }
